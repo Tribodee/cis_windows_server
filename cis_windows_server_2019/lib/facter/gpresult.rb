c98839f407090f2191f48681d89c4f57d1494884
Tribodee <riggker@gmail.com>
2024-02-29 10:48:11 +0700

    add correctly logic custom facts gpresult

--- cis_windows_server_2019/lib/facter/gpresult.rb
+++ cis_windows_server_2019/lib/facter/gpresult.rb
@@ -1,162 +1,48 @@
-#Solution 2
-# Facter.add(:gpresult_facts) do
-#     setcode do
-#         gpresult_output = Facter::Core::Execution.execute('gpresult /r /scope:computer /v')
-#         resultant_set = ""
-#         parsing_resultant_set = false
-#         gpresult_output.each_line do |line|
-#             line_strip = line.strip
-#             parsing_resultant_set = true if line_strip =~ /^Resultant Set Of Policies for Computer/
-#             resultant_set << "#{line_strip}\n" if parsing_resultant_set
-#         end
-#     resultant_set.strip
-#     end
-# end
+require 'facter'
 
-# Solution 7##BEST
-# require 'json'
+Facter.add(:gpresult_facts) do
+    setcode do
+    gpresult_output = Facter::Core::Execution.execute('gpresult /r /scope:computer /v')
+    resultant_sets = []
+    current_set = {}
+    parsing_resultant_set = false
 
-# Facter.add(:gpresult_facts) do
-#     setcode do
+    gpresult_output.each_line do |line|
+        line_strip = line.strip
+        parsing_resultant_set = true if line_strip =~ /^Resultant Set Of Policies for Computer/
+        if parsing_resultant_set
+            if line_strip.empty? || line_strip =~ /^-+$/  # Skip lines with empty or '-' lines
+            # End of the current set, add to the array if it contains valid data
+            computer_setting_keys = current_set.select { |_, v| v.nil? }.keys
+            computer_setting_value = computer_setting_keys.join(',')
+            current_set["Computer Setting"] = computer_setting_value unless computer_setting_value.empty?
+            current_set.delete_if { |_, v| v.nil? }
+            resultant_sets << current_set unless current_set.empty?
+            current_set = {}
+        else
+            # Parse key-value pairs within the set
+            key, value = line_strip.split(':', 2).map(&:strip)
+            current_set[key] = value.nil? ? nil : value.chomp
+        end
+        end
+    end      
 
-#     gpresult_output = Facter::Core::Execution.execute('gpresult /r /scope:computer /v')
-#     resultant_set = []
-#     current_result = {}
+    # Transform each set to have only the value at the head
+    transformed_sets = resultant_sets.map do |set|
+        if set.key?('Folder Id')
+        actual_value = set['Folder Id']
+        set.delete('Folder Id')
+        { actual_value.to_s => set }
+        else
+        actual_value = set['ValueName'] || set['Policy']
+        set.delete('ValueName')
+        set.delete('Policy')
+        { actual_value.to_s => set }
+        end
+    end
 
-#     gpresult_output.each_line do |line|
-#         line_strip = line.strip
-#         if line_strip =~ /^Resultant Set Of Policies for Computer/
-#             current_result = {}
-#         elsif line_strip =~ /^(Policy|ValueName):\s+(.+)$/
-#             key = $1
-#             value = $2.strip
-#             current_result[key] = [value]
-#         elsif line_strip.empty? && !current_result.empty?
-#             resultant_set << current_result
-#             current_result = {}
-#         elsif line_strip.empty? && current_result.empty?
-#         elsif current_result.key?('Policy')
-#             current_result['Policy'] << line_strip.strip
-#         end
-#     end
-    
-#     resultant_set << current_result unless current_result.empty?
-#     JSON.pretty_generate(resultant_set)
-#     end
-# end
-
-#Solution 8 ##Almost complete
-# require 'json'
-
-# Facter.add(:gpresult_facts) do
-#     setcode do
-#     gpresult_output = Facter::Core::Execution.execute('gpresult /r /scope:computer /v')
-#     resultant_set = []
-#     current_result = {}
-#     gpresult_output.each_line do |line|
-#         line_strip = line.strip
-#         if line_strip =~ /^Resultant Set Of Policies for Computer/
-#             current_result = {}
-#         elsif line_strip =~ /^(Policy|ValueName):\s+(.+)$/
-#             key = $1
-#             value = $2.strip
-#             current_result[key] = value
-#         elsif line_strip.empty? && !current_result.empty?
-#             resultant_set << current_result
-#             current_result = {}
-#         elsif line_strip.empty? && current_result.empty?
-#             # Skip empty lines without any data
-#         elsif current_result.key?('Policy')
-#             # If 'Policy' key already exists, append the value to the existing array
-#             current_result['Policy'] << line_strip.strip
-#         end
-#     end
-#     # Include the last result if there's any
-#     resultant_set << current_result unless current_result.empty?
-#     # resultant_set.to_json
-#     JSON.pretty_generate(resultant_set)
-#     end
-# end
-
-#can make =>
-# require 'json'
-
-# Facter.add(:gpresult_facts) do
-#   setcode do
-#     gpresult_output = Facter::Core::Execution.execute('gpresult /r /scope:computer /v')
-#     resultant_set = []
-#     current_result = {}
-
-#     gpresult_output.each_line do |line|
-#       line_strip = line.strip
-
-#       if line_strip =~ /^Resultant Set Of Policies for Computer/
-#         current_result = {}
-#       elsif line_strip =~ /^(Policy|ValueName):\s+(.+)$/
-#         key = $1
-#         value = $2.strip
-#         current_result[key] = value
-#       elsif line_strip.empty? && !current_result.empty?
-#         resultant_set << current_result
-#         current_result = {}
-#       elsif line_strip.empty? && current_result.empty?
-#         # Skip empty lines without any data
-#       elsif current_result.key?('Policy')
-#         # If 'Policy' key already exists, append the value to the existing array
-#         current_result['Policy'] << line_strip.strip
-#       end
-#     end
-
-#     # Include the last result if there's any
-#     resultant_set << current_result unless current_result.empty?
-
-#     # Replace colons with rockets in the resulting JSON
-#     json_result = JSON.pretty_generate(resultant_set).gsub(/":/, '" =>')
-
-#     json_result
-#   end
-# end
-
-
-# #almost
-# require 'json'
-
-# Facter.add(:gpresult_facts) do
-#   setcode do
-#     gpresult_output = Facter::Core::Execution.execute('gpresult /r /scope:computer /v')
-#     resultant_set = []
-#     current_result = {}
-
-#     gpresult_output.each_line do |line|
-#       line_strip = line.strip
-
-#       if line_strip =~ /^Resultant Set Of Policies for Computer/
-#         current_result = {}
-#       elsif line_strip =~ /^(Policy|ValueName):\s+(.+)$/
-#         key = $1
-#         value = $2.strip
-#         if key == 'Policy'
-#           current_result[value] = {}
-#         elsif key == 'ValueName'
-#           current_result[current_result.keys.last]['ValueName'] = value
-#         end
-#       elsif line_strip.empty? && !current_result.empty?
-#         resultant_set << current_result
-#         current_result = {}
-#       elsif line_strip.empty? && current_result.empty?
-#         # Skip empty lines without any data
-#       elsif !current_result.empty? && current_result.key?('Policy')
-#         # If 'Policy' key already exists, append the value to the existing array
-#         current_result[current_result.keys.last]['ComputerSetting'] = line_strip.strip
-#       end
-#     end
-
-#     # Include the last result if there's any
-#     resultant_set << current_result unless current_result.empty?
-
-#     # Replace colons with rockets in the resulting JSON
-#     json_result = JSON.pretty_generate(resultant_set).gsub(/":/, '" =>')
-
-#     json_result
-#   end
-# end
+    # Combine all transformed sets into a single hash
+    combined_hash = transformed_sets.reduce({}, :merge)
+    combined_hash
+    end
+end
