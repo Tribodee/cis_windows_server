d48e2aaeb877b7dc8c2b4f75cf918f0785e350a0
Tribodee <riggker@gmail.com>
2024-03-28 09:27:16 +0700

    remove test process

--- cis_windows_server_2019/manifests/rules/test2.pp
+++
@@ -1,7 +0,0 @@
-#Testing Modules auditpolicy
-class cis_windows_server_2019::rules::test2 {
-  exec {'test007':
-    unless => cis_windows_server_2019::check_auditpol('User Account Management','Success'),
-    command => cis_windows_server_2019::check_auditpol_value('User Account Management'),
-  }
-}
